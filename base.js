5be5cc44de89e263aad4c47042b1e5e4e4482001
Noa <31667392+noacubestudio@users.noreply.github.com>
2023-04-19 23:34:10 +0200

    curve right effect

--- base.js
+++ base.js
@@ -5,13 +5,13 @@ let inputText = '';
 const validKeysForTextInput = 'an';
 
 // font details
-const unitSize = 41;
-const unitsUpperHalf = 3;
-const unitsLowerHalf = 3;
-const unitsColumnHeight = 6;
-const unitsColumnWidth = 3;
-const unitsColumnGap = 1;
-const oneColumnWideLetters = ['i', 'l', ' ', 'j'];
+const stepSize = 41;
+const ysUpper = 3;
+const ysLower = 3;
+const ysColumn = 6;
+const xsColumn = 3;
+const xsGap = 1;
+const oneColumnWideLetters = ['i', 'l', ' ', 'j', '.'];
 const threeColumnWideLetters = ['m', 'w'];
 
 // Load the font file
@@ -52,16 +52,16 @@ function drawInputText(text, font, x, y) {
     let totalColumns = 0;
 
     // draw the top and bottom halves of letters
-    ctx.translate(x * unitSize, y * unitSize);
+    ctx.translate(x * stepSize, y * stepSize);
     text.split("").forEach((char) => {
-        drawLetter(char, font, totalColumns * (unitsColumnWidth + unitsColumnGap), 0);
+        drawLetter(char, font, totalColumns * (xsColumn + xsGap), 0);
         const letterColumnsWidth = (oneColumnWideLetters.includes(char)) ? 1 : ((threeColumnWideLetters.includes(char)) ? 3 : 2);
         totalColumns += letterColumnsWidth;
     });
 
     // columns start higher up
-    ctx.translate(0, unitsLowerHalf * unitSize);
-    drawColumns(0, totalColumns, 'repeat');
+    ctx.translate(0, ysLower * stepSize);
+    drawColumns(0, totalColumns, 'curveRight');
 
     ctx.restore();
 }
@@ -92,7 +92,7 @@ function drawLetter(char, font, x, y) {
     });
 
     // Draw the path on the canvas
-    ctx.translate(x * unitSize, y * unitSize);
+    ctx.translate(x * stepSize, y * stepSize);
     path.fill = 'white';
     path.draw(ctx);
 
@@ -100,16 +100,46 @@ function drawLetter(char, font, x, y) {
 }
 
 function drawColumns(start, count, type) {
+    if (count === undefined || count < 1) return;
+
     ctx.save();
 
-    ctx.translate((unitsColumnWidth+unitsColumnGap) * unitSize * start, 0);
+    ctx.translate((xsColumn+xsGap) * stepSize * start, 0);
+
+    ctx.rect(0, 0, (count * xsColumn + (count-1)*xsGap)*stepSize, ysColumn*stepSize)
+    ctx.clip();
 
     if (type === 'repeat') {
         for (let i = 0; i < count; i++) {
             ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, unitsColumnWidth * unitSize, unitsColumnHeight * unitSize);
+            ctx.fillRect(0, 0, xsColumn * stepSize, ysColumn * stepSize);
 
-            ctx.translate((unitsColumnWidth+unitsColumnGap) * unitSize * 1, 0);
+            ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
+        }
+    } else if (type === "curveRight") {
+        ctx.translate((xsColumn+xsGap) * stepSize * -1, 0);
+        for (let i = 0; i < count+1; i++) {
+            ctx.fillStyle = 'white';
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            //go right
+            ctx.lineTo(xsColumn*stepSize, 0);
+            //go up
+            ctx.bezierCurveTo(
+                (xsColumn                 )*stepSize, ysColumn*stepSize*0.5, 
+                (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*0.5,
+                (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*1
+            );
+            //go left
+            ctx.lineTo((xsColumn+xsGap)*stepSize, ysColumn*stepSize*1);
+            //go down
+            ctx.bezierCurveTo(
+                (xsColumn+xsGap)*stepSize, ysColumn*stepSize*0.5,
+                0, ysColumn*stepSize*0.5,
+                0, 0
+            );
+            ctx.fill();
+            ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
         }
     }
 
