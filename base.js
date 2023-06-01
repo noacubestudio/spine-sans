e0c830af88c005dd7beb9728843393306e8cdcbd
Noa <31667392+noacubestudio@users.noreply.github.com>
2023-06-01 16:56:14 +0200

    switch to svg format for font

--- base.js
+++ base.js
@@ -1,147 +1,213 @@
-let canvas; let ctx; let guiTextInput;
+let canvasEl; let ctx; 
+let textInputEl;
+
+// wip
+//let buffer3d; let buffer2d;
+
 
 // input
-let inputText = '';
-const validKeysForTextInput = 'an';
+let inputText = 'aa';
+const skipColumnDrawingChars = ". ";
+
+// load fonts into this
+const loadedFonts = {
+    bold: {},
+    double: {}
+};
 
 // font details
-const stepSize = 41;
-const ysUpper = 3;
-const ysLower = 3;
-const ysColumn = 6;
-const xsColumn = 3;
-const xsGap = 1;
-const oneColumnWideLetters = ['i', 'l', ' ', 'j', '.'];
-const threeColumnWideLetters = ['m', 'w'];
-
-// Load the font file
-opentype.load('fonts/Columna_Bold_WIP_v1.ttf', function(err, font) {
-    if (err) {
-        alert('Could not load font: ' + err);
-        return;
+const fontMetrics = {
+    bold: {
+        halfHeight: 3.5,
+        colMultiplier: 1,
+        colWidth: 3,
+        colGap: 1,
+    },
+    double: {
+        halfHeight: 3.5,
+        colMultiplier: 2,
+        colWidth: 1,
+        colGap: 1,
     }
+}
 
+// later, different buffers will have their own parameters?
+// but those are set...
+// also, I need to have an a/b set to lerp between? or at least swap out with animation
+const parameters = {
+    colHeight: 1,
+    fontSize: 20
+}
+
+
+// load p5 first
+window.setup = () => {
+    // p5 setup
+    //buffer2d = createGraphics(1360, 800);
+    //buffer3d = createGraphics(1360, 800, WEBGL);
+
+    // load json 
+    fetch('./fonts/SpineSans_Bold_svg.json')
+    .then((response) => response.json())
+    .then((json) => jsonLoaded(json, "bold"));
+}
+
+function jsonLoaded(json, fontNameKey) {
     // gui
-    canvas = document.getElementById('canvas');
+    canvasEl = document.getElementById('canvas');
 
-    guiTextInput = document.getElementById("inputArea");
-    guiTextInput.addEventListener("input", (e) => {
-        inputText = guiTextInput.value;
-        redraw(font);
+    textInputEl = document.getElementById("inputArea");
+    textInputEl.addEventListener("input", (e) => {
+        inputText = textInputEl.value;
+        redraw();
     });
 
     // draw
-    ctx = canvas.getContext('2d');
-    ctx.scale(1, -1);
-    ctx.translate(0, -canvas.height);
+    ctx = canvasEl.getContext('2d');
 
-    inputText = guiTextInput.value;
-    redraw(font);
-});
+    //json
+    loadedFonts[fontNameKey] = json;
 
-function redraw(font) {
+    inputText = textInputEl.value;
+    redraw();
+}
+
+function redraw() {
+    // background
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
-    drawInputText(inputText, font, 2, 4);
+    // draw text and columns
+    ctx.fillStyle = 'white';
+    drawText(50, 70, inputText, "bold", parameters);
+
+    // buffer test drawing
+    // buffer2d.background(255);
+    // buffer2d.fill(0);
+    // buffer2d.rect(100, 100, 200, 200);
+
+    // buffer3d.fill(0);
+    // buffer3d.box(100);
+
+    // ctx.drawImage(buffer3d.canvas, 0, 0, 1360, 800);
 }
 
-function drawInputText(text, font, x, y) {
+function drawText(x, y, text, style, parameters) {
     ctx.save();
+    ctx.translate(x, y);
+    
+    const scale = parameters.fontSize || 30;
+    ctx.scale(scale, scale);
 
-    let totalColumns = 0;
+    const charObjArray = charObjArrayFromString(text);
 
-    // draw the top and bottom halves of letters
-    ctx.translate(x * stepSize, y * stepSize);
-    text.split("").forEach((char) => {
-        drawLetter(char, font, totalColumns * (xsColumn + xsGap), 0);
-        const letterColumnsWidth = (oneColumnWideLetters.includes(char)) ? 1 : ((threeColumnWideLetters.includes(char)) ? 3 : 2);
-        totalColumns += letterColumnsWidth;
+    // draw the halves
+    charObjArray.forEach((charObj) => {
+        // draw
+        drawGlyphHalves(charObj.char, style, parameters.colHeight);
+
+        // advance to next character
+        const styleMetrics = fontMetrics[style];
+        const advanceWidth = charObj.columns * (styleMetrics.colWidth + styleMetrics.colGap) * styleMetrics.colMultiplier;
+        ctx.translate(advanceWidth, 0);
     });
 
-    // columns start higher up
-    ctx.translate(0, ysLower * stepSize);
-    drawColumns(0, totalColumns, 'curveRight');
-
+    // draw the columns
+    // WIP...
+    
     ctx.restore();
 }
 
-function drawLetter(char, font, x, y) {
-    ctx.save();
-
-    // Get the glyph for the letter 'A'
-    const glyph = font.charToGlyph(char);
-    console.log(char, glyph);
-
-    // Create a new path object
-    const path = new opentype.Path();
-
-    // Add the glyph outlines to the path object
-    glyph.path.commands.forEach(function(command) {
-        if (command.type === 'M') {
-            path.moveTo(command.x, command.y);
-        } else if (command.type === 'L') {
-            path.lineTo(command.x, command.y);
-        } else if (command.type === 'Q') {
-            path.quadraticCurveTo(command.x1, command.y1, command.x, command.y);
-        } else if (command.type === 'C') {
-            path.curveTo(command.x1, command.y1, command.x2, command.y2, command.x, command.y);
-        } else if (command.type === 'Z') {
-            path.closePath();
-        }
-    });
-
-    // Draw the path on the canvas
-    ctx.translate(x * stepSize, y * stepSize);
-    path.fill = 'white';
-    path.draw(ctx);
-
-    ctx.restore();
+function getColumnCount(char) {
+    return "wm".includes(char) ? 3
+        : "ijltf. ".includes(char) ? 1
+        : 2; // Default value if not found in single or triple column sets
 }
 
-function drawColumns(start, count, type) {
-    if (count === undefined || count < 1) return;
+function charObjArrayFromString(text) {
+    return [...text].map((c) => {
+        return {
+            char: c, 
+            columns: getColumnCount(c)
+        } 
+    });
+}
 
-    ctx.save();
-
-    ctx.translate((xsColumn+xsGap) * stepSize * start, 0);
-
-    ctx.rect(0, 0, (count * xsColumn + (count-1)*xsGap)*stepSize, ysColumn*stepSize)
-    ctx.clip();
-
-    if (type === 'repeat') {
-        for (let i = 0; i < count; i++) {
-            ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, xsColumn * stepSize, ysColumn * stepSize);
-
-            ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
-        }
-    } else if (type === "curveRight") {
-        ctx.translate((xsColumn+xsGap) * stepSize * -1, 0);
-        for (let i = 0; i < count+1; i++) {
-            ctx.fillStyle = 'white';
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            //go right
-            ctx.lineTo(xsColumn*stepSize, 0);
-            //go up
-            ctx.bezierCurveTo(
-                (xsColumn                 )*stepSize, ysColumn*stepSize*0.5, 
-                (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*0.5,
-                (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*1
-            );
-            //go left
-            ctx.lineTo((xsColumn+xsGap)*stepSize, ysColumn*stepSize*1);
-            //go down
-            ctx.bezierCurveTo(
-                (xsColumn+xsGap)*stepSize, ysColumn*stepSize*0.5,
-                0, ysColumn*stepSize*0.5,
-                0, 0
-            );
-            ctx.fill();
-            ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
-        }
+function drawGlyphHalves(char, style, colHeight) {
+    // get the svg data
+    const svgGlyphObj = loadedFonts[style].characters[char];
+    if (svgGlyphObj === undefined) {
+        console.log(char + " was not found in the font object");
+        ctx.translate(4, 0);
+        return;
     }
 
+    ctx.save();
+
+    // draw top
+    ctx.save();
+    if (svgGlyphObj.top.up !== undefined) ctx.translate(0, -Number(svgGlyphObj.top.up));
+    if (svgGlyphObj.top.left !== undefined) ctx.translate(-Number(svgGlyphObj.top.left), 0);
+    svgGlyphObj.top.paths.forEach((path) => ctx.fill(new Path2D(path)));
+    ctx.restore();
+
+    // go down top half height and column height
+    ctx.translate(0, fontMetrics[style].halfHeight + colHeight);
+
+    // draw bottom
+    ctx.save();
+    if (svgGlyphObj.bottom.left !== undefined) ctx.translate(-Number(svgGlyphObj.bottom.left), 0);
+    svgGlyphObj.bottom.paths.forEach((path) => ctx.fill(new Path2D(path)));
+    ctx.restore();
+
     ctx.restore();
 }
+
+
+
+// function drawColumns(start, count, type) {
+//     if (count === undefined || count < 1) return;
+
+//     ctx.save();
+
+//     ctx.translate((xsColumn+xsGap) * stepSize * start, 0);
+
+//     ctx.rect(0, 0, (count * xsColumn + (count-1)*xsGap)*stepSize, ysColumn*stepSize)
+//     ctx.clip();
+
+//     if (type === 'repeat') {
+//         for (let i = 0; i < count; i++) {
+//             ctx.fillStyle = 'white';
+//             ctx.fillRect(0, 0, xsColumn * stepSize, ysColumn * stepSize);
+
+//             ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
+//         }
+//     } else if (type === "curveRight") {
+//         ctx.translate((xsColumn+xsGap) * stepSize * -1, 0);
+//         for (let i = 0; i < count+1; i++) {
+//             ctx.fillStyle = 'white';
+//             ctx.beginPath();
+//             ctx.moveTo(0, 0);
+//             //go right
+//             ctx.lineTo(xsColumn*stepSize, 0);
+//             //go up
+//             ctx.bezierCurveTo(
+//                 (xsColumn                 )*stepSize, ysColumn*stepSize*0.5, 
+//                 (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*0.5,
+//                 (xsColumn*2+xsGap)*stepSize, ysColumn*stepSize*1
+//             );
+//             //go left
+//             ctx.lineTo((xsColumn+xsGap)*stepSize, ysColumn*stepSize*1);
+//             //go down
+//             ctx.bezierCurveTo(
+//                 (xsColumn+xsGap)*stepSize, ysColumn*stepSize*0.5,
+//                 0, ysColumn*stepSize*0.5,
+//                 0, 0
+//             );
+//             ctx.fill();
+//             ctx.translate((xsColumn+xsGap) * stepSize * 1, 0);
+//         }
+//     }
+
+//     ctx.restore();
+// }
